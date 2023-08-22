3d29dfface9e5e98efcc8326c948cbc18169cc97
bryan-ronad <bryan.ronad@forcepoint.com>
2023-08-22 15:40:56 +0530

    fix: destructured application class members

--- headers/Application.h
+++ headers/Application.h
@@ -7,35 +7,26 @@ using namespace std;
 Application Form
 */
 class Application {
-  private:
-    struct PropertyDetails {
-      string propertyName;
-      int propertyCost;
-    };
-    struct IncomeDetails {
-      int salary;
-    };
-    struct DocumentDetails {
-      string PAN;
-      string Aadhaar;
-    };
-    enum status { PENDING, REJECTED, APPROVED }; 
+private:
+  enum status { PENDING, REJECTED, APPROVED }; 
 
+    string propertyName;
+    int propertyCost;
+    int salary;
+    string PAN;
+    string Aadhaar;
     int applicationID;
     enum status applicationStatus;
-    PropertyDetails propertyDetails; 
-    IncomeDetails incomeDetails;
-    DocumentDetails documentDetails;
 
   public:
     inline static int applicationCount = 0;
 
     Application(string propertyName, int propertyCost, int salary, string PAN, string Aadhaar, status=PENDING){
-      propertyDetails.propertyName = propertyName;
-      propertyDetails.propertyCost = propertyCost;
-      incomeDetails.salary = salary;
-      documentDetails.PAN = PAN;
-      documentDetails.Aadhaar = Aadhaar;
+      propertyName = propertyName;
+      propertyCost = propertyCost;
+      salary = salary;
+      PAN = PAN;
+      Aadhaar = Aadhaar;
       srand((unsigned) time(NULL));
       applicationID = rand(); 
       ++applicationCount;
