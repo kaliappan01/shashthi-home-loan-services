15fc8acae31c2352256a1e9dd52241c4f6135382
kaliappan-yadav <kaliappan.yadav@forcepoint.com>
2023-09-04 20:23:20 +0530

    cleaned up unwanted files

--- test/testLoanService.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../layers/service/Loan_Application_service.h"
-#include<fstream>
-int main(){
-    
-    Loan_Application_service loan_app_service_obj =Loan_Application_service();
-    loan_app_service_obj.fetchAllApplications(1,true);
-    return 1;
-}
